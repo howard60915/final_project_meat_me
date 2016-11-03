46bae2925c6c6bf9acf41c88dab14dcad0654761
jacobshan <jacobshan429@gmail.com>
2016-11-03 19:20:27 +0800

    generated Site model and made association with User

---
+++ db/migrate/20161103111852_create_sites.rb
@@ -0,0 +1,13 @@
+class CreateSites < ActiveRecord::Migration[5.0]
+  def change
+    create_table :sites do |t|
+      t.string :name
+      t.string :address
+      t.string :tel
+      t.string :duration
+      t.boolean :hotspot
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103111307) do
+ActiveRecord::Schema.define(version: 20161103111852) do
+
+  create_table "sites", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "tel"
+    t.string   "duration"
+    t.boolean  "hotspot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "email",                                default: "", null: false
