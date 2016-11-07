50a4b6e24b03371b796455e5b0af3003d61f3a1e
jacobshan <jacobshan429@gmail.com>
2016-11-07 16:36:41 +0800

    added delete pictures functionality

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -23,6 +23,29 @@ class Admin::SitesController < Admin::BaseController
     end
   end
 
+  def edit
+    @site = Site.find(params[:id])
+  end
+
+  def update
+    @site = Site.find(params[:id])
+    if params[:destroy_picture] == '1'
+      @site.pictures.destroy_all
+    end
+    if @site.update(params_site)
+      flash[:notice] = "修改成功！"
+      if params[:images]
+        params[:images].each { |image|
+          @site.pictures.create(image: image)
+        }
+      end
+      redirect_to admin_sites_path
+    else
+      flash[:alert] = "修改失敗！"
+      render 'edit'
+    end
+  end
+
   private
   def params_site
     params.require(:site).permit(:name, :address, :tel, :hotspot, :pictures, :duration)
