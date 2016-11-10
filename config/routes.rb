596d4a548cba5f15211b29d39af22a1eac9624d7
jacobshan <jacobshan429@gmail.com>
2016-11-10 17:12:27 +0800

    added welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root :to => 'admin/index#index'
+  root :to => 'welcome#index'
 
   namespace :admin do
     root :to => "index#index"
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :users
   end
 
-  scope :path => "/api/v1", :module => "api/v1", :as => "v1", :defaults => {:format => :json} do 
+  scope :path => "/api/v1", :module => "api/v1", :as => "v1", :defaults => {:format => :json} do
     post "login" => "auth#login"
     post "logout" => "auth#logout"
 
