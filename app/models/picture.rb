26ebc8cfa3b5df3ad6a86c45f80da6dcb03ab3fa
jacobshan <jacobshan429@gmail.com>
2016-11-12 16:59:42 +0800

    remove code comment

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,8 +3,6 @@ class Picture < ApplicationRecord
 
   has_attached_file :image,
     styles: { medium: "300x300>", thumb: "100x100>" }
-    # :path => ":rails_root/public/images/:id/:filename",
-    # :url  => "/images/:id/:filename"
 
   do_not_validate_attachment_file_type :image
 end
