017355369fe20f3a4330b8091dd197cb3b32b1ac
jacobshan <jacobshan429@gmail.com>
2016-11-12 16:45:52 +0800

    fix pic url bug

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,9 +2,9 @@ class Picture < ApplicationRecord
   belongs_to :site
 
   has_attached_file :image,
-    styles: { medium: "300x300>", thumb: "100x100>" },
-    :path => ":rails_root/public/images/:id/:filename",
-    :url  => "/images/:id/:filename"
+    styles: { medium: "300x300>", thumb: "100x100>" }
+    # :path => ":rails_root/public/images/:id/:filename",
+    # :url  => "/images/:id/:filename"
 
   do_not_validate_attachment_file_type :image
 end
