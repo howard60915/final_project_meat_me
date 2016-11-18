cd957253d2749aa88048fb727c671161c42e6b5f
jacobshan <jacobshan429@gmail.com>
2016-11-18 15:36:48 +0800

    added comment author image

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,11 +6,17 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   def api_info
+    avatarUrl = [
+      'https://c5.staticflickr.com/6/5568/22842328868_5eedf8e61a.jpg',
+      'https://c3.staticflickr.com/6/5645/22842328338_4e0ef78181.jpg',
+      'https://c1.staticflickr.com/6/5451/22842327648_1321a9b888.jpg'
+    ]
     nameList = ["OhNO", "Chrisrtine", "Justin", "Jacob", "Howard"]
     {
       :articleCommentId => self.id,
       :articleCommentContent => self.content,
       :articleCommentAuthor => nameList.sample,
+      :articleCommentAuthorImage => avatarUrl.sample,
       :articleCommentDate => self.created_at.strftime("%Y.%m.%d")
     }
   end
