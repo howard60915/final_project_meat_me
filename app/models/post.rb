573e28c2361e5fa1cd91f954b3e0846fec246eaf
jacobshan <jacobshan429@gmail.com>
2016-11-19 21:12:32 +0800

    add post author image

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,11 +11,17 @@ class Post < ApplicationRecord
 
   def api_info
     nameList = ["OhNO", "Chrisrtine", "Justin", "Jacob", "Howard"]
+    avatarUrl = [
+      'https://c5.staticflickr.com/6/5568/22842328868_5eedf8e61a.jpg',
+      'https://c3.staticflickr.com/6/5645/22842328338_4e0ef78181.jpg',
+      'https://c1.staticflickr.com/6/5451/22842327648_1321a9b888.jpg'
+    ]
     return {
       :articleId => self.id.to_s,
       :articleTitle => self.title,
       :articleContent => self.content,
       :articleAuthorName => nameList.sample,
+      :articleAuthorImage => avatarUrl,
       :articleImage => "#{SITE_DOMAIN}#{self.photo.url}",
       :articleDate => self.created_at.strftime("%Y.%m.%d"),
       :articleLoveNumber => (10..30).to_a.sample,
